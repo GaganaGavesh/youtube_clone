0331f0d593550cb9cbfe26a5cdbbea33bb3e0d22
Gagana Gavesh <gaveshgamage@gmail.com>
2022-09-06 08:10:20 +0530

    Added video detail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Box, Stack, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 function VideoDetail() {
-  return <div>VideoDetail</div>;
+  const { id: videoId } = useParams();
+  const [videoData, setVideoData] = useState(null);
+
+  useEffect(() => {
+    fetchFromAPI(
+      `videos?part=contentDetails,snippet,statistics&id=${videoId}`
+    ).then((data) => {
+      setVideoData(data.items[0]);
+    });
+  }, [videoId]);
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              className="react-player"
+              url={`https://www.youtube.com/watch?v=${videoId}`}
+              controls
+            />
+            <Typography color="#FFF" variant="h5" fontWeight="bold" p={2}>
+              {videoData.snippet.title}
+            </Typography>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 }
 
 export default VideoDetail;
