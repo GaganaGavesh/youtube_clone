5da58364b03ff49978b7aaf5322200325d261511
Gagana Gavesh <gaveshgamage@gmail.com>
2022-09-06 08:13:05 +0530

    COnfigured the search bar to search items

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,21 @@ import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
   //Paper is like a div which has white background
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //override the default behavior of the browser when submitting form values
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -20,8 +31,10 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
       />
       <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
         <Search />
